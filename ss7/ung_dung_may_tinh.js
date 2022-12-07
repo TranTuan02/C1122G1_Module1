566fc310ac2ec9980a2fcf63cb5b99b033f3d819
unknown <you@example.com>
2022-12-07 12:35:10 +0700

    update

--- ss7/ung_dung_may_tinh.js
+++ ss7/ung_dung_may_tinh.js
@@ -1,29 +1,32 @@
-
-function Cộng() {
+function Cong() {
     let numb1 = +document.getElementById("numb1").value;
     let numb2 = +document.getElementById("numb2").value;
     let result = numb1 + numb2;
-    document.getElementById("result").innerHTML ="Kết quả là : " + result;
+
+    document.getElementById("result").innerHTML = "Kết quả là : " + result;
 
 }
-function Trừ() {
+
+function Tru() {
     let numb1 = document.getElementById("numb1").value;
     let numb2 = document.getElementById("numb2").value;
     let result = numb1 - numb2;
-    document.getElementById("result").innerHTML ="Kết quả là  : " + result;
+    document.getElementById("result").innerHTML = "Kết quả là  : " + result;
 
 }
-function Nhân() {
+
+function Nhan() {
     let numb1 = document.getElementById("numb1").value;
     let numb2 = document.getElementById("numb2").value;
     let result = numb1 * numb2;
-    document.getElementById("result").innerHTML ="Kết quả là  : " + result;
+    document.getElementById("result").innerHTML = "Kết quả là  : " + result;
 
 }
+
 function Chia() {
     let numb1 = document.getElementById("numb1").value;
     let numb2 = document.getElementById("numb2").value;
     let result = numb1 / numb2;
-    document.getElementById("result").innerHTML ="Kết quả là  : " + result;
+    document.getElementById("result").innerHTML = "Kết quả là  : " + result;
 
 }
