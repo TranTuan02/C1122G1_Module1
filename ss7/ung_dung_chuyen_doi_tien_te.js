f64adeff847dd05ea0fa0049bc9af358f966f23c
unknown <you@example.com>
2022-12-07 11:50:01 +0700

    update

--- ss7/ung_dung_chuyen_doi_tien_te.js
+++ ss7/ung_dung_chuyen_doi_tien_te.js
@@ -2,16 +2,18 @@ function ChangeMoney() {
     let FromCurrency=document.getElementById("From").value;
     let ToCurrency=document.getElementById("To").value;
     let Amount = document.getElementById("Amount").value;
-
-    if (FromCurrency == "USD" && Amount == "VND") {
+    alert("amount la "+Amount)
+    alert("from la "+FromCurrency)
+    alert("to la "+ToCurrency)
+    let result = 0;
+    if (FromCurrency == "USD" && ToCurrency == "VND") {
         result = " result " + (Amount * 23000) + "VND";
-    } else if (FromCurrency == "VND" && Amount == "USD") {
+    } else if (FromCurrency == "VND" && ToCurrency == "USD") {
         result = "result" + (Amount / 23000) + "USD";
     } else if (FromCurrency == "VND") {
         result = "result" + Amount + "VND";
     } else if (FromCurrency == "USD") {
         result = "result" + Amount + "USD";
     }
-    document.getElementById("money").innerHTML = result;
-}
+        document.getElementById("kQ").innerText = result;
 }
